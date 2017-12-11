f0b0b56eca2af69d040fa9c01c4e5ad44bcfd751
Frederic Vandeborne <fre@novation.be>
2017-12-11 09:50:38 +0100

    Rewrote Company and Home logic

--- src/Company.js
+++ src/Company.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import Highlighter from 'react-highlight-words';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import { withRouter } from 'react-router-dom';
 
-export default class Company extends React.Component {
+class Company extends React.Component {
+
+    onActive = (e) => {
+        if(e.pseudoElement === '::after' && this.props.active ) {
+            this.props.history.push(`/company/${this.props.id}`);
+        }
+    };
     
     render() {
         
         const img = (this.props.logo) ? <img src={this.props.logo} alt={this.props.name} /> : '';
+
+        let classList = classNames({
+            'Company': true,
+            'Company--active': this.props.active
+        });
         
         return (
 
-            <div className="Company" draggable="false">
-                <NavLink to={`/company/${this.props.id}/${this.props.slug}`} className="Company__link">
-                    <div className="Company__logo">
-                        { img }
-                    </div>
-                    <div className="Company__name">
-                        <Highlighter
-                            highlightClassName="highlight"
-                            textToHighlight={this.props.name}
-                            searchWords={[ this.props.highlight ]} />
-                    </div>
-                </NavLink>
+            <div className={classList} draggable="false" onTransitionEnd={this.onActive} onClick={() => this.props.onClick(this.props.id)}>
+                <div className="Company__logo">
+                    { img }
+                </div>
+                <div className="Company__name">
+                    <Highlighter
+                        highlightClassName="highlight"
+                        textToHighlight={this.props.name}
+                        searchWords={[ this.props.highlight ]} />
+                </div>
             </div>
         );
     }
 }
+
+export default withRouter(Company);
--- src/Home.js
+++ src/Home.js
@@ -1,65 +1,45 @@
 import React from 'react';
-import fuzzysearch from 'fuzzysearch';
-import {Switch, Route} from 'react-router';
+// import fuzzysearch from 'fuzzysearch';
+// import {Switch, Route} from 'react-router';
 
 import accent_folder from './accent-fold';
 
 import Title from './Title';
 
-import { client } from './client';
 import CompanyDashboard from './CompanyDashboard'
 
 import Letterbar from './Letterbar'
 import Search from './Search'
-
 import Style from './Filter.css';
 
 
 export default class Home extends React.Component {
     state = {
-        companies: [],
-        filteredCompanies: [],
-        searchQuery: ''
+        searchQuery: '',
     };
 
-    filter = (e) => {
+    // filter = (e) => {
 
-        console.log('test');
+    //     this.setState({
+    //         searchQuery: e.target.value
+    //     });
 
-        this.setState({
-            searchQuery: e.target.value
-        });
-
-        const value = accent_folder(e.target.value.toLowerCase());
+    //     const value = accent_folder(e.target.value.toLowerCase());
         
-        const filteredCompanies = this.state.companies.filter( company => {
+    //     const filteredCompanies = this.state.companies.filter( company => {
 
-            const name = accent_folder(company.name);
-            const employees = company.employees && company.employees.some( medewerker => fuzzysearch( value, accent_folder(`${medewerker.voornaam.toLowerCase()} ${medewerker.achternaam.toLowerCase()}`) ) );
+    //         const name = accent_folder(company.name);
+    //         const employees = company.employees && company.employees.some( medewerker => fuzzysearch( value, accent_folder(`${medewerker.voornaam.toLowerCase()} ${medewerker.achternaam.toLowerCase()}`) ) );
 
-            return fuzzysearch( value, name.toLowerCase() ) || employees;
+    //         return fuzzysearch( value, name.toLowerCase() ) || employees;
 
-        });
+    //     });
 
-        this.setState({
-            filteredCompanies
-        });
+    //     this.setState({
+    //         filteredCompanies
+    //     });
 
-    }
-
-    componentDidMount() {
-        this.fetchCompanies();
-    }
-
-    fetchCompanies() {
-        client.fetchCompanies()
-            .then( companies => {
-                this.setState({
-                    companies,
-                    filteredCompanies: companies
-                });
-            });
-    }
+    // }
 
     render() {
         return (
@@ -69,10 +49,7 @@ export default class Home extends React.Component {
                     <Letterbar />
                     <Search />
                 </div>
-                <CompanyDashboard
-                    highlight={this.state.searchQuery}
-                    companies={this.state.filteredCompanies}
-                />
+                <CompanyDashboard highlight={this.state.searchQuery} />
                 {/* <div className="companies">
                     <div className="companies__search">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>

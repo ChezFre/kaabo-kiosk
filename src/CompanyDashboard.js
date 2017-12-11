2bbba10b330480e21f810307b9cfe56d846532bc
Frederic Vandeborne <fre@novation.be>
2017-12-11 09:50:53 +0100

    Moved State management down to CompanyDashboard

--- src/CompanyDashboard.js
+++ src/CompanyDashboard.js
@@ -2,23 +2,57 @@ import React from 'react';
 import Company from './Company'
 import Style from './CompanyDashboard.css';
 
-const CompanyDashboard = (props) => (
-    <ul className="CompanyDashboard">
-        {props.companies.map( company => {
-            return (
-                <li key={company.id} className="CompanyDashboard__company">
-                    <Company
-                        id={company.id}
-                        className="Company"
-                        logo={company.logo}
-                        name={company.name}
-                        slug={company.slug}
-                        highlight={props.highlight}
-                    />
-                </li>
-            );
-        })}
-    </ul>
-);
+import { client } from './client';
+
+class CompanyDashboard extends React.Component {
+    
+    state = {
+        companies: [],
+        filteredCompanies: [],
+        activeCompany: undefined
+    };
+
+    componentDidMount() {
+        this.fetchCompanies();
+    }
+
+    fetchCompanies() {
+        client.fetchCompanies()
+            .then(companies => {
+                this.setState({
+                    companies,
+                    filteredCompanies: companies
+                });
+            });
+    }
+
+    handleCompanyClick = (id) => {
+        this.setState({ activeCompany: id });
+    }
+
+    render() {
+        return (
+            <ul className="CompanyDashboard">
+                {this.state.companies.map(company => {
+                    return (
+                        <li key={company.id} className="CompanyDashboard__company">
+                            <Company
+                                id={company.id}
+                                className="Company"
+                                logo={company.logo}
+                                name={company.name}
+                                slug={company.slug}
+                                highlight={this.props.highlight}
+                                active={this.state.activeCompany === company.id}
+
+                                onClick={this.handleCompanyClick}
+                            />
+                        </li>
+                    );
+                })}
+            </ul>
+        )
+    }
+}
 
 export default CompanyDashboard;
